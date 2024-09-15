822e82c18da72ab90348e19299d7924ba2c2d835
Ailton2023 <141515952+Ailton2023@users.noreply.github.com>
2024-09-15 18:27:42 -0300

    dddd

---
+++ Aula 12 – Condições (Parte 2)/aula.js
@@ -0,0 +1,7 @@
+var idade = 24
+if (idade <18){
+    console.log('Menor de Idade')
+}
+else{
+    console.log('Maior de Idade')
+}
---
+++ Aula 12 – Condições (Parte 2)/aula_b.js
@@ -0,0 +1,13 @@
+var idade = 66
+    console.log(`Vivendo em ${idade}`)
+if (idade <16){
+    console.log('Não vota!')
+}
+else if (idade <18 || idade >65){ // || -> OU
+    console.log('Voto opcional')
+}
+else {
+    console.log('Voto obrigatório')
+}
+    
+ 
