0c9651390cb05d4e617c9348b8725fc8456b833e
Ailton2023 <141515952+Ailton2023@users.noreply.github.com>
2024-09-20 17:56:16 -0300

    tt

--- Aula 12 – Condições (Parte 2)/codigos_exercicio_parte_I.js
+++ Aula 12 – Condições (Parte 2)/codigos_exercicio_parte_I.js
@@ -5,19 +5,21 @@
  var img = window.document.getElementById('imagem')
  var data = new Date()
  //var hora = data.getHours()
- var hora = 17
+ var hora = 20
  msg.innerHTML = `Agora são ${hora} horas.`
 
  if(hora >= 0 && hora < 12){
     //Bom dia!
     img.src = 'relogio-02.png'
+    document.body.style.background = '#e2cd9f'
  } else if (hora >=12 && hora < 18){  
     //Boa tarde!
     img.src = 'computadores-01-250px.png'
     msg2.innerHTML = `Continue seus estudos.`
+    document.body.style.background = '#b9846f'
  } else {
     //Boa noite!
-
+    document.body.style.background = '#515154'
  }
 
  }
